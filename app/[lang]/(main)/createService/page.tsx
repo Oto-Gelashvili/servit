e7866d6347408fbfaa45433fad8eb2e438784d48
otogelashvili <otarbasilevsi@gmail.com>
2025-01-27 17:07:10 +0400

    feat: add dynamic metadata to createService page

--- app/[lang]/(main)/createService/page.tsx
+++ app/[lang]/(main)/createService/page.tsx
@@ -1,7 +1,19 @@
 import { CreateServiceForm } from '../../components/createService/createServiceForm';
 import { Locale } from '../../../../get-dictionaries';
 import './createService.css';
+import { Metadata } from 'next';
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { lang: Locale };
+}): Promise<Metadata> {
+  return {
+    title: params.lang === 'ka' ? 'სერვისის შექმნა' : 'Create Service',
+    description:
+      params.lang === 'ka' ? 'ახალი სერვისის შექმნა' : 'Create a new service',
+  };
+}
 export default function createProduct({
   params,
 }: {
