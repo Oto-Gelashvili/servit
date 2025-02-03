e5158689cdfa62416a73f09a63e52032f65a8d57
otogelashvili <otarbasilevsi@gmail.com>
2025-02-03 16:47:46 +0400

    feat: add home link btn on protected page + improve UI

--- app/[lang]/(auth-pages)/protected/page.tsx
+++ app/[lang]/(auth-pages)/protected/page.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import { getDictionary, Locale } from '../../../../get-dictionaries';
+import Image from 'next/image';
 
 export default async function loggedInPage({
   params,
@@ -7,8 +9,22 @@ export default async function loggedInPage({
 }) {
   const dictionary = await getDictionary(params.lang);
   return (
-    <main className="flex flex-1 items-center justify-center">
-      <h1 className="text-3xl">{dictionary.protected.loggedIn}</h1>
-    </main>
+    <div className="flex flex-col gap-6 items-center justify-center">
+      <Image
+        src="/images/login.svg"
+        alt="Login illustration"
+        width={250}
+        height={250}
+      ></Image>
+      <div className="flex flex-col gap-4 items-center sm:flex-row">
+        <h1 className="text-3xl">{dictionary.protected.loggedIn}</h1>
+        <Link
+          className="bg-[var(--secondary-color)] text-[var(--accent-color)] text-3xl p-4 rounded-lg hover:bg-[var(--primary-color)] hover:text-white transition-all"
+          href={`/${params.lang}/`}
+        >
+          {dictionary.header.home}
+        </Link>
+      </div>
+    </div>
   );
 }
